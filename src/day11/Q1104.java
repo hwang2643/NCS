66a738ca7a4df5feccc79f87f49e139e12da14c8
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-16 17:58:56 +0900

    W3 Day4

---
+++ src/day11/Q1104.java
@@ -0,0 +1,7 @@
+package day11;
+
+public class Q1104 {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/day11/Q1105.java
@@ -0,0 +1,7 @@
+package day11;
+
+public class Q1105 {
+	public static void main(String[] args) {
+		
+	}
+}
