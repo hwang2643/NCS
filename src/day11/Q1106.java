c4002181dffc3cd16614bd808e9bbeb70ed5b7a0
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-16 18:01:28 +0900

    W3 Day4

--- src/day11/Q1106.java
+++ src/day11/Q1106.java
@@ -1,5 +1,12 @@
 package day11;
 
+/*
+ * 연습(최고급).
+	크기가 5인 정수형 배열을 난수 발생(1~9)해서 채우고,
+	1) 배열 전체의 내용을 출력.
+	2) "버블정렬 알고리즘"을 적용해서, 오름차순 정렬 후 출력.
+
+ */
 public class Q1106 {
 	public static void main(String[] args) {
 		
