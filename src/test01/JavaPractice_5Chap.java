11c9695bf6ca494a05fd42a44af8c7dfc76698c1
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-24 16:59:33 +0900

    W4 Day5

--- src/test01/JavaPractice_5Chap.java
+++ src/test01/JavaPractice_5Chap.java
@@ -60,29 +60,39 @@ public class JavaPractice_5Chap {
 //			System.out.println("USAGE: java Exercise5_7 3120");
 //			System.exit(0);
 //			}
-//			// . . 문자열을 숫자로 변환한다 입력한 값이 숫자가 아닐 경우 예외가 발생한다
+			// . . 문자열을 숫자로 변환한다 입력한 값이 숫자가 아닐 경우 예외가 발생한다
 //			int money = Integer.parseInt(args[0]);
-//			System.out.println("money="+money);
-//			int[] coinUnit = {500, 100, 50, 10 }; // 동전의 단위
-//			int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수
-//			for(int i=0;i<coinUnit.length;i++) {
-//				int coinNum = 0;
-//				/* (1) . 아래의 로직에 맞게 코드를 작성하시오
-//				1. 금액 (money) 을 동전단위로 나눠서 필요한 동전의 개수(coinNum) 를 구한다. 
-//				2.배열 coin 에서 coinNum 만큼의 동전을 뺀다
-//				 (만일 충분한 동전이 없다면 배열 coin에 있는 만큼만 뺀다.)
-//				3.금액에서 동전의 개수(coinNum)와 동전단위를 곱한 값을 뺀다.
-//				*/
-//				System.out.println(coinUnit[i]+"원 : "+coinNum); 
-//			}
-//			if(money > 0) {
-//				System.out.println(" 거스름돈이 부족합니다."); 
-//				System.exit(0); //프로그램을 종료한다
-//			}
-//			System.out.println("=남은 동전의 개수 ="); 
-//			for(int i=0;i<coinUnit.length;i++) {
-//				System.out.println(coinUnit[i]+"원 :"+coin[i]); 
-//			}
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("money=");
+		int money = sc.nextInt();
+		int[] coinUnit = {500, 100, 50, 10 }; // 동전의 단위
+		int[] coin = {5, 5, 5, 5}; // 단위별 동전의 개수
+		for(int i=0;i<coinUnit.length;i++) {
+			int coinNum = 0;
+			coinNum = money/coinUnit[i];
+			if(coin[i]>=coinNum) {
+				break;
+			}else if(coin[i]<coinNum){
+				coinNum = coin[i];
+				money -= (coinUnit[i]*coin[i]);
+			}
+			/* (1) . 아래의 로직에 맞게 코드를 작성하시오
+			1. 금액 (money) 을 동전단위로 나눠서 필요한 동전의 개수(coinNum) 를 구한다. 
+			2.배열 coin 에서 coinNum 만큼의 동전을 뺀다
+			(만일 충분한 동전이 없다면 배열 coin에 있는 만큼만 뺀다.)
+			3.금액에서 동전의 개수(coinNum)와 동전단위를 곱한 값을 뺀다.
+			*/
+			System.out.println(coinUnit[i]+"원 : "+coinNum); 
+		}
+		if(money > 0) {
+			System.out.println(" 거스름돈이 부족합니다."); 
+			System.exit(0); //프로그램을 종료한다
+		}
+		System.out.println("=남은 동전의 개수 ="); 
+		for(int i=0;i<coinUnit.length;i++) {
+			System.out.println(coinUnit[i]+"원 :"+coin[i]); 
+		}
 		
 		// 5-8
 //		int[] answer = { 1,4,4,3,1,4,4,2,1,3,2 };
